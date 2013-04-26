613e9982bacca53bc2eca1e380fc7d120d53f407
thenghia.pham <thenghia.pham@unitn.it>
2013-04-26 22:33:33 +0200

    - new tree structure

---
+++ tree_kernel/tree/vector_syntactic_node.py
@@ -0,0 +1,18 @@
+'''
+Created on Apr 26, 2013
+
+@author: nghia
+'''
+from syntactic_node import SyntacticNode
+
+class VectorSyntacticNode(SyntacticNode):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, label, phrase):
+        '''
+        Constructor
+        '''
+        pass
