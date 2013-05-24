f33c8046ded43919ae750031ab5f43f85d87c2f2
thenghia.pham <thenghia.pham@unitn.it>
2013-05-24 09:43:57 +0200

    - tree utils change lemma to lemma-pos

--- tree_kernel/utils/tree_utils.py
+++ tree_kernel/utils/tree_utils.py
@@ -13,6 +13,19 @@ from composes.transformation.scaling.row_normalization import RowNormalization
 import numpy as np
 from composes.matrix.dense_matrix import DenseMatrix
 
+def penn_pos_2_simple_pos(penn_pos):
+    """
+    try:
+        simple_pos = pos_map[penn_pos]
+        
+    except KeyError:
+        simple_pos = "?"
+    """
+    simple_pos = penn_pos[0].lower()
+    if not simple_pos in "abcdefghijklmnopqrstuvwxyz":
+        simple_pos = "x"
+    return simple_pos 
+
 def syntactic_tree_2_semantic_tree(syntactic_tree, vector_space, composition_model, normed=True):
     assert_type(syntactic_tree, SyntacticTree)
     return SemanticTree(_syntactic_node_2_semantic_node(syntactic_tree._root,
@@ -35,7 +48,8 @@ def _syntactic_node_2_semantic_node(syntactic_node, vector_space, composition_mo
                 else:
                     new_node._vector = row_vector
             except KeyError:
-                    new_node._vector = DenseMatrix(np.zeros((1,vector_space.cooccurrence_matrix.shape[1]),dtype=np.float))
+                    new_node._vector = DenseMatrix(np.zeros((1,vector_space.cooccurrence_matrix.shape[1]),
+                                                            dtype=np.float))
         else:
             new_vector = new_node.get_child(0)._vector
             # print new_node
@@ -45,5 +59,21 @@ def _syntactic_node_2_semantic_node(syntactic_node, vector_space, composition_mo
                 new_node._vector = RowNormalization().apply(new_vector)
             else:
                 new_node._vector = new_vector
+        return new_node
+
+def lemma_tree_2_lemmapos_tree(syntactic_tree):
+    assert_type(syntactic_tree, SyntacticTree)
+    return SyntacticTree(_lemma_tree_2_lemmapos_tree(syntactic_tree._root, None))
+
+def _lemma_tree_2_lemmapos_tree(syntactic_node, parent_node):
+    if syntactic_node._type == SyntacticNode.TERMINAL:
+        return SyntacticNode(syntactic_node._label + "-" + penn_pos_2_simple_pos(parent_node._label),
+                             SyntacticNode.TERMINAL)
+    else:
+        new_node = SyntacticNode(syntactic_node._label, syntactic_node._type)
+        for child in syntactic_node._children:
+            new_child = _lemma_tree_2_lemmapos_tree(child, syntactic_node)
+            new_node.add_child(new_child)
+            
         return new_node
     
