670c3f1be74d8bd06fb4f3f5adea25a959d8a2ab
thenghia.pham <thenghia.pham@unitn.it>
2013-06-02 06:43:13 +0200

    - add special case v1.norm = 0 or v2.norm to sentence_vector_kernel

--- tree_kernel/kernel/sentence_vector_kernel.py
+++ tree_kernel/kernel/sentence_vector_kernel.py
@@ -23,5 +23,10 @@ class SentenceVectorKernel(Kernel):
     def dot_product(self, tree1, tree2):
         assert_type(tree1, SemanticTree)
         assert_type(tree2, SemanticTree)
-        return self._similarity(tree1._root._vector, tree2._root._vector)
+        sentence_vector1 = tree1._root._vector
+        sentence_vector2 = tree2._root._vector
+        if sentence_vector1.norm() == 0.0 or sentence_vector2.norm() == 0.0:
+            return 0.0
+        else:
+            return self._similarity(sentence_vector1, sentence_vector2)
     
