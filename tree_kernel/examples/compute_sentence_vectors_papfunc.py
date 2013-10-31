f2bceb280ab01f02dc5063856ddd2d0713d99834
you <you@localhost.localdomain>
2013-10-31 16:31:10 +0100

    now accepting all xml files in a given folder as input

--- tree_kernel/examples/compute_sentence_vectors_papfunc.py
+++ tree_kernel/examples/compute_sentence_vectors_papfunc.py
@@ -2,13 +2,14 @@ from __future__ import print_function
 import sys
 from xml.etree.ElementTree import ParseError
 import re
+import os
 from tree.papfunc import Papfunc_SemanticNode
 from tree.semantic_node import SemanticNode
 from tree.syntactic_tree import SyntacticTree
 from composes.semantic_space.space import Space
 
 #this scripts takes 4 arguments: 
-#*input xml file
+#*directory of input xml files
 #*output file
 #*vector space prefix (for dm format)
 #*matrix spce prefix (for dm format)
@@ -27,59 +28,57 @@ matspace = Space.build(data = matfilepref + ".dm",
                        rows = matfilepref + ".rows",
                        format = "dm")
 
-infile = sys.argv[1]
-outfile= open(sys.argv[2],'w')
-matfile=open(sys.argv[2]+".matreps",'w')
+for infile in os.listdir(sys.argv[1]):
+ if (re.search (r'\.xml',infile)):
+  print("processing %s ..." %infile)
+  outname=sys.argv[2]+"."+infile
+  outfile= open(outname+".dm",'w')
+  matfile=open(outname+".matreps",'w')
+  
+  sent=0
+  succ=0
+  vecs=0
+  
+  currtree=""
+  intree=0
 
-sent=0
-succ=0
-vecs=0
-
-currtree=""
-intree=0
-with open(infile) as data:
- for line in data:
-  if re.match("^<ccg>",line):
-   currtree = currtree + line
-   intree=1
-   sent +=1
-  elif re.match("^</ccg>",line):
-   currtree = currtree + line
-   # print current tree
-   #print("sentence %s" %sent)
-   #print(currtree.strip('\n'),file=outfile)
-   intree=0
-   try:
-    syntactic_tree = SyntacticTree.parse_tree_from_xml_string(currtree)
-    succ +=1
-    # print top node 
-    semnode = SemanticNode.create_semantic_node(syntactic_tree.root,None)
-    papnode = Papfunc_SemanticNode.create_papfunc_node(semnode,vecspace,matspace)
-    #print(papnode,end='',file=outfile)
-    #for x in (papnode.get_vector()[0]): print(x)#(x,file=outfile)
-    #print(papnode.get_numrep())
-    try:
-      #for x in numpy.array(papnode.get_vector().flatten()): print(str(x)+"\tJesus")
-      for x in range(len(papnode.get_vector().mat.A[0].tolist())):
-       dim=str(papnode.get_vector().mat.A[0].tolist()[x])
-       if x>0: print('\t',end='',file=outfile)
-       print(dim,end='',file=outfile)
-      print(papnode.get_matrep(),file=matfile)
-      print("",file=outfile)
-      vecs +=1
-    except AttributeError: print ("Papnode %s doesn't have a vector representation" %papnode)
-#   for x in semnode.get_numrep(): print(x,file=outfile)
-   except ParseError:
-    "THIS TREE WAS NOT XML-PARSABLE: %s" %currtree
-
-   # reset current tree
-   currtree=""
-
-  elif intree:
-   currtree = currtree + line
-print("%i sentences transgressed" %sent)
-print("%i sentences successfully parsed" %succ)
-print("%i vectors produced" %vecs)
-data.close()
-outfile.close()
-matfile.close()
+#process each xml file in the input directory
+  with open(sys.argv[1]+infile) as data:
+   for line in data:
+    if re.match("^<ccg>",line):
+     currtree = currtree + line
+     intree=1
+     sent +=1
+    elif re.match("^</ccg>",line):
+     currtree = currtree + line
+     intree=0
+     try:
+      syntactic_tree = SyntacticTree.parse_tree_from_xml_string(currtree)
+      succ +=1
+      semnode = SemanticNode.create_semantic_node(syntactic_tree.root,None)
+      papnode = Papfunc_SemanticNode.create_papfunc_node(semnode,vecspace,matspace)
+      try:
+        for x in range(len(papnode.get_vector().mat.A[0].tolist())):
+         dim=str(papnode.get_vector().mat.A[0].tolist()[x])
+         if x>0: print('\t',end='',file=outfile)
+         print(dim,end='',file=outfile)
+        print(papnode.get_matrep(),file=matfile)
+        print("",file=outfile)
+        vecs +=1
+      except AttributeError: print ("Papnode %s doesn't have a vector representation" %papnode)
+     except ParseError:
+      "THIS TREE WAS NOT XML-PARSABLE: %s" %currtree
+  
+    # reset current tree
+     currtree=""
+  
+    elif intree:
+     currtree = currtree + line
+  print("%i sentences transgressed" %sent)
+  print("%i sentences successfully parsed" %succ)
+  print("%i vectors produced" %vecs)
+  data.close()
+  outfile.close()
+  matfile.close()
+ 
+print("Done.")
