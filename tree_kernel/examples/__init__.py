6c6f10809b0790c3066f630723a3376c336dd811
thenghia.pham <thenghia.pham@unitn.it>
2014-07-22 16:30:15 +0200

    - crap

---
+++ tree_kernel/examples/__init__.py
@@ -0,0 +1,4 @@
+import os
+current_dir = os.path.dirname(os.path.realpath(__file__))
+test_vector_file_prefix = "%s/resource/vectors" %current_dir
+test_matrix_file_prefix = "%s/resource/matrices" %current_dir
--- tree_kernel/examples/test_papfunc.py
+++ tree_kernel/examples/test_papfunc.py
@@ -2,14 +2,17 @@ from tree.papfunc import Papfunc_SemanticNode
 from tree.semantic_node import SemanticNode
 from tree.syntactic_tree import SyntacticTree
 from composes.semantic_space.space import Space
+from examples import test_vector_file_prefix, test_matrix_file_prefix
+
+# FIRST TEST
 xml_string = '''
 <ccg>
 <lf start="0" span="1" word="dog" lemma="dog" pos="NN" chunk="I-NP" entity="O" cat="N" />
 </ccg>
 '''
 syntactic_tree = SyntacticTree.parse_tree_from_xml_string(xml_string)
-vecfilepref = "/home/pham/work/project/lf/test_md/vectors"
-matfilepref = "/home/pham/work/project/lf/test_md/matrices"
+vecfilepref = test_vector_file_prefix
+matfilepref = test_matrix_file_prefix
 vecspace = Space.build(data = vecfilepref + ".dm",
                        rows = vecfilepref + ".rows",
                        format = "dm")
@@ -18,27 +21,15 @@ matspace = Space.build(data = matfilepref + ".dm",
                        format = "dm")
 
 semnode = SemanticNode.create_semantic_node(syntactic_tree.root,None)
-
-#print semnode
-#print [semnode]
-#print semnode.is_terminal()
-
-#for child in semnode._children: 
-# print child
-# print [child]
-# print child.is_terminal()
-# print child._children
-
-#testnode=Papfunc_SemanticNode("magic",None)
-#print testnode
-
-print semnode.is_terminal()
 papnode = Papfunc_SemanticNode.create_papfunc_node(semnode,vecspace,matspace)
 
-
-print papnode._matrep
+print "*****"
+print "Syntactic tree:", semnode
+print "Symbolic representation:", papnode._matrep
+print "Numeric representation:"
 for x in papnode._numrep: print x
 
+# SECOND TEST
 xml_string = '''
 <ccg>
  <rule type="fa" cat="NP[nb]">
@@ -52,10 +43,12 @@ xml_string = '''
 '''
 
 syntactic_tree = SyntacticTree.parse_tree_from_xml_string(xml_string)
-
 semnode = SemanticNode.create_semantic_node(syntactic_tree.root,None)
-
 papnode = Papfunc_SemanticNode.create_papfunc_node(semnode,vecspace,matspace)
 
+print "*****"
+print "Syntactic tree:", semnode
+print "Symbolic representation:", papnode._matrep
+print "Numeric representation:"
 print papnode._matrep
 for x in papnode._numrep: print x
